9cbf803147f355b1aadf605a206909e80a703b77
naielv <109038805+naielv@users.noreply.github.com>
2023-05-07 15:48:06 +0000

    Added base modules

--- js/app.js
+++ js/app.js
@@ -1,16 +1,66 @@
 const container = document.querySelector(".container");
+const module_photo = "e";
 
-const main = () => {
-  container.innerHTML = "Esta APP se puede instalar en un movil, tablet o ordenador.";
-};
-
-document.addEventListener("DOMContentLoaded", main);
+function Module(module_url, v) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("GET", module_url, false); // false for synchronous request
+  xmlHttp.send(null);
+  const text = xmlHttp.responseText;
+  const result_1 = eval("`" + text + "`");
+  return String(result_1);
+}
+function index() {
+  var card_photo = Module("modules/photo.html", {
+    source: "img/coffee.jpg",
+    alt: "Photo of Coffee",
+  });
+  var card1 = Module("modules/card.html", {
+    photo: card_photo,
+    title: "Café Corazon",
+    text: "Cafe riquisimo con dibujo de corazon",
+    btn: {
+      url: "/",
+      text: "Test",
+    },
+  });
+  var card2 = Module("modules/card.html", {
+    photo: card_photo,
+    title: "Café Corazon",
+    text: "Cafe riquisimo con dibujo de corazon",
+    btn: {
+      url: "#",
+      text: "Test",
+    },
+  });
+  var card3 = Module("modules/card.html", {
+    photo: card_photo,
+    title: "Café Corazon",
+    text: "Cafe riquisimo con dibujo de corazon",
+    btn: {
+      url: "#",
+      text: "Test",
+    },
+  });
+  var card4 = Module("modules/card.html", {
+    photo: card_photo,
+    title: "Café Corazon",
+    text: "Cafe riquisimo con dibujo de corazon",
+    btn: {
+      url: "#",
+      text: "Test",
+    },
+  });
+  var grid = Module("modules/grid.html", {
+    content: card1 + card2 + card3 + card4,
+  });
+  container.innerHTML = grid;
+}
 
 if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
+  window.addEventListener("load", function () {
     navigator.serviceWorker
       .register("/serviceWorker.js")
-      .then(res => console.log("service worker registered"))
-      .catch(err => console.log("service worker not registered", err));
+      .then((res) => console.log("service worker registered"))
+      .catch((err) => console.log("service worker not registered", err));
   });
 }
--- serviceWorker.js
+++ serviceWorker.js
@@ -3,22 +3,22 @@ const assets = [
   "/",
   "/index.html",
   "/css/style.css",
-  "/js/app.js"
- // "/images/icon/icon.png"
+  "/js/app.js",
+  // "/images/icon/icon.png"
 ];
 
-self.addEventListener("install", installEvent => {
+self.addEventListener("install", (installEvent) => {
   installEvent.waitUntil(
-    caches.open(staticCache).then(cache => {
+    caches.open(staticCache).then((cache) => {
       cache.addAll(assets);
     })
   );
 });
 
-self.addEventListener("fetch", fetchEvent => {
+self.addEventListener("fetch", (fetchEvent) => {
   fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request);
+    caches.match(fetchEvent.request).then((res) => {
+      return fetch(fetchEvent.request) || res;
     })
   );
 });
