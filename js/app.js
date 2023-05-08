9a6c642d14d46925706976a66bab4f2a2cf1386b
naielv <109038805+naielv@users.noreply.github.com>
2023-05-08 18:18:44 +0000

    V3.5 - Separated modules into functions that returns elements instead of HTML

--- js/app.js
+++ js/app.js
@@ -1,47 +1,108 @@
 const container = document.querySelector(".container");
 const module_photo = "e";
 const urlParams = new URLSearchParams(window.location.search);
-
-function Module(module_url, v) {
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.open("GET", "/modules/" + module_url, false); // false for synchronous request
-  xmlHttp.send(null);
-  const text = xmlHttp.responseText;
-  const result_1 = eval("`" + text + "`");
-  return String(result_1);
-}
+//////// Modulos
 function JSONGet(json_url) {
   var xmlHttp = new XMLHttpRequest();
   xmlHttp.open("GET", "/json/" + json_url, false); // false for synchronous request
   xmlHttp.send(null);
   return JSON.parse(xmlHttp.responseText);
 }
+function ModButton(label, onclick) {
+  let btn = document.createElement("button");
+  btn.innerText = label;
+  btn.className = "btn";
+  btn.onclick = onclick;
+  return btn;
+}
+function ModButtonLink(label, url) {
+  let btn = document.createElement("a");
+  btn.innerText = label;
+  btn.className = "btn";
+  btn.href = url;
+  return btn;
+}
+function ModPhoto(url, alt) {
+  let photo = document.createElement("img");
+  photo.src = url;
+  photo.alt = alt;
+  return photo;
+}
+function ModPDF(url, width, height) {
+  let pdf = document.createElement("iframe");
+  pdf.src = url;
+  pdf.style.width = width;
+  pdf.style.height = height;
+  return pdf;
+}
+function ModTitle(text) {
+  let t = document.createElement("h2");
+  t.innerText = text;
+  return t;
+}
+function ModText(text) {
+  let t = document.createElement("p");
+  t.innerText = text;
+  return t;
+}
+function ModInput(id, type, placeholder, label) {
+  let div = document.createElement("div");
+  let lab = document.createElement("label");
+  let inp = document.createElement("input");
+  inp.type = type;
+  inp.id = id;
+  inp.placeholder = placeholder;
+  lab.innerText = label;
+  div.appendChild(lab);
+  div.appendChild(inp);
+  return div;
+}
+function ModOutput(label, id) {
+  let div = document.createElement("div");
+  let lab = document.createElement("label");
+  let h3 = document.createElement("h3");
+  h3.id = id;
+  h3.className = "output";
+  lab.innerText = label;
+  div.appendChild(lab);
+  div.appendChild(h3);
+  return div;
+}
+function ModCard(nodes) {
+  let div = document.createElement("div");
+  div.className = "card";
+  nodes.forEach((node) => {
+    div.appendChild(node);
+  });
+  return div;
+}
+function ModGrid(nodes) {
+  let div = document.createElement("div");
+  div.className = "grid";
+  nodes.forEach((node) => {
+    div.appendChild(node);
+  });
+  return div;
+}
 
+//////// INICIO
 function index() {
-  let out = "";
+  let cards = [];
   const apps = JSONGet("apps.json");
   apps.forEach((element) => {
-    var photo = Module("photo.html", {
-      source: "/img/app_icons/" + element.img,
-      alt: element.name,
-    });
-    var card = Module("card.html", {
-      photo: photo,
-      title: element.name,
-      text: element.details,
-      btn: {
-        url: element.url,
-        text: "Acceder",
-      },
-    });
-    out += card;
+    cards.push(
+      ModCard([
+        ModPhoto("/img/app_icons/" + element.img, element.name),
+        ModTitle(element.name),
+        ModText(element.details),
+        ModButtonLink("Acceder", element.url),
+      ])
+    );
   });
-  var grid = Module("grid.html", {
-    content: out,
-  });
-  container.innerHTML = grid;
+  var grid = ModGrid(cards);
+  container.appendChild(grid);
 }
-//////// APP KCal: Nivel de Calorias ////////
+//////// APP KCal: Nivel de Calorias
 function app_kcal_calcular() {
   const inp = document.getElementById("inp_kcal");
   const ovl = document.getElementById("out_kcal");
@@ -61,68 +122,43 @@ function app_kcal_calcular() {
   ovl.innerHTML = out;
 }
 function app_kcal() {
-  var input1 = Module("input.html", {
-    id: "inp_kcal",
-    label: "KCal",
-    type: "number",
-    placeholder: "Introduce un numero...",
-  });
-  var button = Module("button.html", {
-    label: "Calcular",
-    onclick: "app_kcal_calcular()",
-  });
-  var output = Module("output.html", {
-    label: "Resultado",
-    id: "out_kcal",
-  });
-  var card1 = Module("card.html", {
-    title: "Nivel de calorias",
-    text: "Calculadora para saber el nivel de calorias",
-    footer: input1 + button + output,
-  });
-  var grid = Module("grid.html", {
-    content: card1,
-  });
-  container.innerHTML = grid;
+  var grid = ModGrid([
+    ModCard([
+      ModTitle("Nivel de Calorias"),
+      ModText("Calculadora para saber el nivel de calorias"),
+      ModInput("inp_kcal", "number", "Introduce un numero...", "KCal"),
+      ModButton("Calcular", app_kcal_calcular),
+      ModOutput("Resultado", "out_kcal"),
+    ]),
+  ]);
+  container.appendChild(grid);
 }
-//////// APP Books: Biblioteca digital ////////
+//////// APP Books: Biblioteca digital
 
 function app_books() {
   const view = urlParams.has("book");
   const book = urlParams.get("book");
-  let out = "";
   const books = JSONGet("books.json");
 
   if (view) {
     const selbook = books.find(({ id }) => id === book);
-    var pdf = Module("pdf.html", {
-      source: selbook.pdf,
-      width: "100%",
-      height: "750vh"
-    });
-    out = pdf
+    var pdf = ModPDF(selbook.pdf, "100%", "90vh");
+    container.appendChild(pdf);
   } else {
+    let cards = [];
     books.forEach((element) => {
-      var photo = Module("photo.html", {
-        source: "/img/app_books/" + element.img,
-        alt: element.name,
-      });
-      var card = Module("card.html", {
-        photo: photo,
-        title: element.name,
-        text: element.details,
-        btn: {
-          url: "/app/books.html?book=" + element.id,
-          text: "Ver Libro",
-        },
-      });
-      out += card;
+      cards.push(
+        ModCard([
+          ModPhoto("/img/app_books/" + element.img, element.name),
+          ModTitle(element.name),
+          ModText(element.details),
+          ModButtonLink("Ver Libro", "/app/books.html?book=" + element.id),
+        ])
+      );
     });
+    var grid = ModGrid(cards);
+    container.appendChild(grid);
   }
-  var grid = Module("grid.html", {
-    content: out,
-  });
-  container.innerHTML = grid;
 }
 
 if ("serviceWorker" in navigator) {
